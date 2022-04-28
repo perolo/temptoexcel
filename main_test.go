7a0bfdc47821410c8484826d08658940a8e24fbd
Lenovo <per.e.olofsson@gmail.com>
2022-04-28 08:13:02 +0200

    minimize

--- main_test.go
+++ main_test.go
@@ -1,15 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 )
 
 func Test_Main_Dummytest(t *testing.T) {
-
-	str := fmt.Sprintf("Dummy string \n") //nolint:gosimple
-	if str != "Dummy string \n" {
-		t.Error(str)
-	}
 
 }
